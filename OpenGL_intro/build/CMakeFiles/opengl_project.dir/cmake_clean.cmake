c43eb4d85b352a75dd6abcd041d62fd3b0d5c707
HaydenMcCleary <hayden0pm@gmail.com>
2024-05-10 01:01:38 -0400

    added set up directory

---
+++ OpenGL_intro/build/CMakeFiles/opengl_project.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/opengl_project.dir/src/glad.c.o"
+  "CMakeFiles/opengl_project.dir/src/glad.c.o.d"
+  "CMakeFiles/opengl_project.dir/src/main.cpp.o"
+  "CMakeFiles/opengl_project.dir/src/main.cpp.o.d"
+  "opengl_project"
+  "opengl_project.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/opengl_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
